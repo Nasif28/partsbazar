5bee786598485dce828318094678084f839db43a
Nasif28 <nasifzeehan1@gmail.com>
2025-07-10 12:36:19 +0600

    Product Section

--- src/app/(frontend)/page.jsx
+++ src/app/(frontend)/page.jsx
@@ -6,20 +6,23 @@ import Features from "@/components/Home/Features";
 import HeroBanner from "@/components/Home/Hero/HeroBanner";
 import TopBrands from "@/components/Home/TopBrands";
 import TopProducts from "@/components/Home/TopProducts";
+import { Suspense } from "react";
 // import Products from "@/components/Products/Products";
 
 export default function HomePage() {
   return (
     <div>
-      <HeroBanner />
-      <Features />
-      <TopBrands />
-      <BlogSlider />
-      {/* <Products /> */}
-      <TopProducts />
-      {/* <CategorizedProducts /> */}
-      <CategoryTab />
-      <Prod />
+      <Suspense fallback={<div>Loading...</div>}>
+        <HeroBanner />
+        <Features />
+        <TopBrands />
+        <BlogSlider />
+        {/* <Products /> */}
+        <TopProducts />
+        {/* <CategorizedProducts /> */}
+        <CategoryTab />
+        <Prod />
+      </Suspense>
     </div>
   );
 }
