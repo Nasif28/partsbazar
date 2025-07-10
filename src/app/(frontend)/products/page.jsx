c73ec9d21a5de1726721a8d165caaceec4a4aea3
Nasif28 <nasifzeehan1@gmail.com>
2025-07-10 12:31:05 +0600

    Product Section

--- src/app/(frontend)/products/page.jsx
+++ src/app/(frontend)/products/page.jsx
@@ -1,3 +1,45 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>page</div>
+    // import ProductFilter from "@/components/Products/ProductFilter";
+    // import Products from "@/components/Products/Products";
+    // import Prod from "@/components/Products/Prod";
+    // import React from "react";
+
+    // const ProductsPage = () => {
+    //   return (
+    //     <div>
+    //       <ProductFilter />
+    //       <Products />
+    //       <Prod />
+    //     </div>
+    //   );
+    // };
+
+    // export default ProductsPage;
+  );
+};
+// import ProductFilter from "@/components/Products/ProductFilter";
+// import Products from "@/components/Products/Products";
+// import Prod from "@/components/Products/Prod";
+// import React from "react";
+
+// const ProductsPage = () => {
+//   return (
+//     <div>
+//       <ProductFilter />
+//       <Products />
+//       <Prod />
+//     </div>
+//   );
+// };
+
+// export default ProductsPage;
+
+export default page;
+
 // import ProductFilter from "@/components/Products/ProductFilter";
 // import Products from "@/components/Products/Products";
 // import Prod from "@/components/Products/Prod";
