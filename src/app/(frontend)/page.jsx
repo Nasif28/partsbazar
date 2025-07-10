7fce819632d4d8cb53dd082742dfb33cb0f31a8e
Nasif28 <nasifzeehan1@gmail.com>
2025-07-10 12:33:18 +0600

    Product Section

--- src/app/(frontend)/page.jsx
+++ src/app/(frontend)/page.jsx
@@ -1,12 +1,12 @@
 import BlogSlider from "@/components/Home/BlogSlider";
-import CategorizedProducts from "@/components/Home/CategorizedProducts/CategorizedProducts";
+// import CategorizedProducts from "@/components/Home/CategorizedProducts/CategorizedProducts";
 import Prod from "@/components/Products/Prod";
 import CategoryTab from "@/components/Home/CategorizedProducts/CategoryTab";
 import Features from "@/components/Home/Features";
 import HeroBanner from "@/components/Home/Hero/HeroBanner";
 import TopBrands from "@/components/Home/TopBrands";
 import TopProducts from "@/components/Home/TopProducts";
-import Products from "@/components/Products/Products";
+// import Products from "@/components/Products/Products";
 
 export default function HomePage() {
   return (
