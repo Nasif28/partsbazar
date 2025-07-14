e9aefb7681a778351853dc6acf82a9d7e10bbe19
Nasif28 <nasifzeehan1@gmail.com>
2025-07-14 16:10:38 +0600

    update config

--- next.config.mjs
+++ next.config.mjs
@@ -2,15 +2,41 @@
 // const path = require("path");
 const nextConfig = {
   images: {
-    domains: [
-      "res.cloudinary.com",
-      "example.com",
-      "partschai.com",
-      "japanparts.com.bd",
-      "m.media-amazon.com",
-      "cdn.pixabay.com",
-      "i.ytimg.com",
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+      {
+        protocol: "https",
+        hostname: "example.com",
+      },
+      {
+        protocol: "https",
+        hostname: "partschai.com",
+      },
+      {
+        protocol: "https",
+        hostname: "japanparts.com.bd",
+      },
+      {
+        protocol: "https",
+        hostname: "m.media-amazon.com",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.pixabay.com",
+      },
+      {
+        protocol: "https",
+        hostname: "i.ytimg.com",
+      },
     ],
+
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    formats: ["image/webp"],
+    minimumCacheTTL: 60,
   },
   experimental: {
     // serverActions: true,
